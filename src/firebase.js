ea2598159329b2f84fc8d2ea451641cba8ac573f
sarif manung <406168026@yru.ac.th>
2022-01-09 12:10:39 +0700

    set and delete

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYuBjsCffCAEsha7Ch2-IfWGTvKNsWCcw",
+    authDomain: "netflix-clone-sarif.firebaseapp.com",
+    projectId: "netflix-clone-sarif",
+    storageBucket: "netflix-clone-sarif.appspot.com",
+    messagingSenderId: "141442939998",
+    appId: "1:141442939998:web:9c74a83dac19a5024448cf",
+    measurementId: "G-H3TSCN8C4E"
+  };
