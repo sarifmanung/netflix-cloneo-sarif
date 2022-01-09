e67a72947238976f2262363329d3949194cb59a3
sarif manung <406168026@yru.ac.th>
2022-01-09 19:48:57 +0700

    add

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -17,6 +17,7 @@ function Banner() {
     }
     fetchData();
   }, []);
+  console.log(movie);
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
