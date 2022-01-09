51f6517cdfb9f305385b2ddd46e12848c9d0d4b7
sarif manung <406168026@yru.ac.th>
2022-01-09 22:50:47 +0700

    change icon

--- src/component/Row.js
+++ src/component/Row.js
@@ -19,18 +19,23 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
   return (
     <div className="row">
       <h2>{title}</h2>
+      
       <div className="row_posters">
         {" "}
-        {movies.map((movie) => (
-          <img
-            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            key={movie.id}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movies.backdrop_path
-            }`}
-            alt=""
-          />
-        ))}
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movies.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
       </div>
     </div>
   );
